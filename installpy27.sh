55c18dd235a7fde869959fb0f814bfcd615085e4
Baiju Muthukadan <baiju.m.mail@gmail.com>
2012-06-17 17:48:43 +0530

    Alternate download directory for offline install

--- installpy27.sh
+++ installpy27.sh
@@ -3,7 +3,13 @@ PYTHON="Python-$VERSION"
 MD5="c57477edd6d18bd9eeca2f21add73919"
 PREFIX="`pwd`/usr"
 DOWNLOAD_PREFIX="http://www.python.org/ftp/python/$VERSION"
-DOWNLOADDIR="downloads"
+
+if [[ -e "release-distributions/$PYTHON.tar.bz2" ]]
+    then
+    DOWNLOADDIR="release-distributions"
+else
+    DOWNLOADDIR="downloads"
+fi
 
 mkdir -p $DOWNLOADDIR
 cd $DOWNLOADDIR
